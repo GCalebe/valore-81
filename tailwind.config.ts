78ed2ead3b25c7d4f236a6d88bad8efd32085145
GABRIEL CALEBE DA SILVA ALVES <74987559+GCalebe@users.noreply.github.com>
2025-06-04 16:52:53 -0300

    Add files via upload

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -25,7 +25,7 @@ export default {
 				ring: 'hsl(var(--ring))',
 				background: 'hsl(var(--background))',
 				foreground: 'hsl(var(--foreground))',
-				petshop: {
+				Valore: {
 					blue: '#1a365d',
 					gold: '#fbbf24',
 					navy: '#172554',
